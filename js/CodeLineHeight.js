d7de5fae015a6c57662a6ce40b6c79781e7c45e8
honjun <hj531@live.com>
2018-04-09 01:05:31 +0800

    Site updated: 2018-04-09 01:05:25

--- js/CodeLineHeight.js
+++ js/CodeLineHeight.js
@@ -2,7 +2,8 @@ $(function(){
 	var codeline = $('.code .line');
 	var gutter = $('.gutter .line');
 	var codeLength = codeline.length;
-	for (var i=0; i<codeLength; i++) {
+	for (var i = 0; i < codeLength; i++) {
+    console.log(codeline.get(i).offsetHeight);
 		gutter.get(i).style.height = codeline.get(i).offsetHeight + 'px';
 	}
 });
