96b7c7ab25c31728805003ae20f05999112cc3bd
honjun <hj531@live.com>
2020-10-02 09:14:30 +0800

    Site updated: 2020-10-02 09:11:23

--- js/CodeLineHeight.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	var codeline = $('.code .line');
-	var gutter = $('.gutter .line');
-	var codeLength = codeline.length;
-	for (var i = 0; i < codeLength; i++) {
-		gutter.get(i).style.height = codeline.get(i).offsetHeight + 'px';
-	}
-});
--- js/loading.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-	$("#loading").fadeOut(800);
-});
